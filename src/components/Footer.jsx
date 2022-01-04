9ea670decc1e7ceb6193d86dedcab0aab1952ebb
danielcr0995 <daniel.correa.1995@gmail.com>
2022-01-04 17:33:26 -0500

    added notes sample on file notes.js

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     const year = new Date().getFullYear();
 
-    return <footer> Copyright {year}</footer>
+    return <footer> Copyright  © {year}</footer>
 }
 
-export default Footer;
+export default Footer; 
