14a71857b6a8d2a8a0f87f948d8ac30db352ffce
danielcr0995 <daniel.correa.1995@gmail.com>
2022-01-05 21:02:26 -0500

    added files and styles for stage 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,12 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note"
 import notes from "../notes"
+import CreateArea from "./CreateArea"
 
 function App(){
     return (<div>
                 <Header/>
+                <CreateArea/>
                 {notes.map(note =>(                  
                 <Note   
                     key={note.key}                 
